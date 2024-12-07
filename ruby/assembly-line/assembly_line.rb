1198897179c1f67852a53c3a75d801ad4dfcf522
Shah Nawaz <shuvo1824@hotmail.com>
2024-12-07 23:38:32 +0600

    attempt: ruby - assembly line

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -2,23 +2,27 @@ class AssemblyLine
   def initialize(speed)
     @speed = speed
     @production_rate = 221
-    @success_rate = 100
 
-    if speed > 4 and speed >= 8
-      @success_rate = 90
-    elsif speed == 9
-      @success_rate = 80
-    elsif speed == 10
-      @success_rate = 77
-    end
-  end
+ end
 
   def production_rate_per_hour
-    (@speed * @production_rate) * (@success_rate/100)
+    if @speed > 0 and @speed <= 4
+      success_rate = 100
+    elsif @speed > 4 and @speed <= 8
+      success_rate = 90
+    elsif @speed == 9
+      success_rate = 80
+    elsif @speed == 10
+      success_rate = 77
+    else
+      success_rate = 0
+    end
+
+    (@speed * @production_rate) * (success_rate/100)
   end
 
   def working_items_per_minute
-    per_minute = ((@speed * @production_rate) * (@success_rate/100)) / 60
+    per_minute = production_rate_per_hour / 60
     per_minute.floor
   end
 end
